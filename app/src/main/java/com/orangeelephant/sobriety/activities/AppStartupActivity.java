dfbe53a390d80361076088341e563df89421b432
elena <orangeelephant@tutanota.com>
2022-01-14 20:27:57 +0000

    Add fingerprint unlocking to the app. - For API >=28

--- app/src/main/java/com/orangeelephant/sobriety/activities/AppStartupActivity.java
+++ app/src/main/java/com/orangeelephant/sobriety/activities/AppStartupActivity.java
@@ -1,23 +1,32 @@
 package com.orangeelephant.sobriety.activities;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
+import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.biometrics.BiometricPrompt;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.widget.Toast;
 
 import com.orangeelephant.sobriety.database.DBhelper;
 import com.orangeelephant.sobriety.database.SqlCipherMigration;
 import com.orangeelephant.sobriety.database.SqlcipherKey;
 import com.orangeelephant.sobriety.logging.LogEvent;
+import com.orangeelephant.sobriety.R;
 
 import net.sqlcipher.database.SQLiteDatabase;
 import net.sqlcipher.database.SQLiteException;
 
+import java.util.concurrent.Executor;
+
 public class AppStartupActivity extends AppCompatActivity {
+
     private static final String sharedPreferenceFile = "com.orangeelephant.sobriety.database_key";
     private static final String isEncrypted = "isEncrypted";
     private SharedPreferences sharedPreferences;
@@ -46,8 +55,14 @@ public class AppStartupActivity extends AppCompatActivity {
                 };
         sharedPreferences.registerOnSharedPreferenceChangeListener(preferenceChangeListener);
 
-        Intent intent = new Intent(AppStartupActivity.this, HomeScreenActivity.class);
-        startActivity(intent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            fingerprintUnlock();
+        } else {
+            //start app
+            Intent intent = new Intent(AppStartupActivity.this, HomeScreenActivity.class);
+            startActivity(intent);
+        }
+
     }
 
     @Override
@@ -69,5 +84,60 @@ public class AppStartupActivity extends AppCompatActivity {
         } catch (Exception exception) {
             LogEvent.e("Exception loading sqlcipher key", exception);
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private void fingerprintUnlock() {
+        Executor executor;
+        BiometricPrompt biometricPrompt;
+
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt.Builder(this)
+                .setTitle(this.getString(R.string.AppStartup_unlock_title))
+                .setSubtitle(this.getString(R.string.AppStartup_unlock_subtitle))
+                .setNegativeButton(getString(R.string.AppStartup_unlock_negative_button), executor, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        moveTaskToBack(true);
+                        AppStartupActivity.super.onBackPressed();
+                    }
+                })
+                .build();
+
+        BiometricPrompt.AuthenticationCallback callback = new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                        getApplicationContext().getString(R.string.AppStartup_unlock_error) + errString, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),
+                        getApplicationContext().getString(R.string.AppStartup_unlock_successful),
+                        Toast.LENGTH_SHORT)
+                        .show();
+
+                //start app
+                Intent intent = new Intent(AppStartupActivity.this, HomeScreenActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(),
+                        getApplicationContext().getString(R.string.AppStartup_unlock_failed),
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        };
+
+        biometricPrompt.authenticate(new CancellationSignal(), executor, callback);
     }
 }
