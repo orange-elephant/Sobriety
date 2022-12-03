0a610df6fb5b2fe6a1a07841d8497cbb24fd9820
elena <orangeelephant@tutanota.com>
2022-12-03 21:35:43 +0000

    Support following system default theme

--- app/src/main/java/com/orangeelephant/sobriety/ui/Themes.java
+++ app/src/main/java/com/orangeelephant/sobriety/ui/Themes.java
@@ -1,8 +1,11 @@
 package com.orangeelephant.sobriety.ui;
 
+import android.content.res.Configuration;
+
 import androidx.annotation.StyleRes;
 
 import com.orangeelephant.sobriety.R;
+import com.orangeelephant.sobriety.dependencies.ApplicationDependencies;
 
 public enum Themes {
     SYSTEM,
@@ -11,8 +14,15 @@ public enum Themes {
 
     public @StyleRes int toStyle() {
         switch (this) {
-            case LIGHT_MODE:
             case SYSTEM:
+                int currentNight = ApplicationDependencies.getApplicationContext().
+                        getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+
+                if (currentNight == Configuration.UI_MODE_NIGHT_YES) {
+                    return R.style.Theme_Sobriety_Dark;
+                }
+                return R.style.Theme_Sobriety_Light;
+            case LIGHT_MODE:
                 return R.style.Theme_Sobriety_Light;
             case DARK_MODE:
             default:
